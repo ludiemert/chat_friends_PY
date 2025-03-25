4b346a1bb98c73dce1cc65f5f729e48da53c9644
ludiemert <lucianadiemert@gmail.com>
2025-03-25 14:51:04 +0100

    add step project

---
+++ step.py
@@ -0,0 +1,18 @@
+# criar um sistema, anota todas as telas do projeto
+# Tela inicial
+  # Titulo: Pag inicial
+  # Botao: Start chat
+    # quando clicar no botao:
+    # abrir um popup/modal/alert
+      #  titulo (ola bem vindo)
+      # caixa de texto: escreva um texto
+      # botao: entrar no chat
+        # quando clicar no botao
+        # sumir com o titulo
+        # sumir com o botao de iniciar chat
+          # carregar o chat
+          # carregar o campo de enviar msg: Digite a sua msg
+          # botao Enviar
+            # quando clicar no botao enviar
+            # enviar a msg
+            # limpar a cx de msg
