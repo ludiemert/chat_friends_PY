e5bfa39e334d3dfbbe6350ed35479ac557d16bb8
ludiemert <lucianadiemert@gmail.com>
2025-04-07 22:35:57 +0100

    clear codgo

--- home.py
+++ home.py
@@ -8,13 +8,23 @@ def main(pagina):  #costuma dar nome main para a funcao principal, Obrigatoriame
 
     # titulo
     titulo = ft.Text("Welcome to Chat_friends 🥰", color="purple", size=18) #ft.FERRAMENTA
-    pagina.add(titulo)
+    pagina.add(titulo) # codig coloca esse elemento na pagina, ou colocar tudo no final da pagina
 
 
     # botao inicial
-    botao = ft.ElevatedButton("Start_Chat🥳", color="blue")
-    pagina.add(botao)
+
+    #funcao do botao (on_click= open_popup) a funcao vai dizer oque vai acontecer qdo o usuario clicar no botao
+    def open_popup(evento): # ele obrigatoriamente recebe o evento do botao
+        print("clicou no botao") # sempre que clicar no botao sera um botao e um evento
+
+    botao = ft.ElevatedButton("Start_Chat🥳", color="blue", on_click=open_popup)  #acao (funcao) do botao on_click,
+    pagina.add(botao) # codig coloca esse elemento na pagina, ou colocar tudo no final da pagina
+
+    # exemplo colocar os elementos que vai ter na pagina
+    #pagina.add(titulo)
+    #paginaadd(botao)
+
 
 # executar essa funcao com o flet
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER) #abre formato web
 
