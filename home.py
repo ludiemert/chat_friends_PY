aaaec7a8f988d5c606ed4ee4ab9c161a4cf752a2
ludiemert <lucianadiemert@gmail.com>
2025-04-10 12:28:38 +0100

    add tunnel_conumication

--- home.py
+++ home.py
@@ -8,16 +8,41 @@ def main(pagina):  # Costuma dar nome 'main' para a função principal. Obrigato
     titulo = ft.Text("Welcome to Chat_friends  🥰", color="purple", size=18)
     pagina.add(titulo)  # Código coloca esse elemento na página, ou colocar tudo no final da página
 
+    # websocket => comunicacao entre 2 usuarios = um tunel de comunicacao para as pessoas se comunicar e conversarem no chat
+    # websocket - tunel de comunicacao entre 2 user
+    # 1 - criar uma funcao para o tunel
+    def send_msg_tunnel(msg): #evento, criando a funcao que vai aparecer no tunel de comunicacao
+        # enviar tudo oque eu quero que aconteca para TODOS os usuarios que receberem a msg
+        # aconteca para TODOS os usuarios, criei o tunel, pego a msg do user envio no tunel
+        text = ft.Text(msg) # crio um texto na tela de todos os usuarios
+        chat.controls.append(text) # add texto na tela de todos users
+        pagina.update() # atualizar a pagina de todos users para aparecer no chat de todos
+
+    # criar o tunel, o flet da o nome de pubsub para esse tunel de comunicacao
+    pagina.pubsub.subscribe(send_msg_tunnel) # pega a aplicacao (pagina) crio um tunel de comunicacao (pubsub) e estou dizendo para o tunel de comunicacao sempre que algo foi dito no tunel (subscribe) voce enviar essa funcao (send_msg_tunnel). #  passa a funcao que acabou de criar
+    # sao 3 coisas que precisa para acontecer um tunel de comunicacao
+        # 1 - criar uma funcao para o tunel, para todos os users
+        # 2 - criar o tunel de comunicacao, o flet da o nome de pubsub
+        # 3 - enviar uma msg para o tunel de comunicacao
+            # ao inves de fazer isso :
+                # text = ft.Text(f"{name_user} : {text_camp_msg}") #colocar valores dinamicos no PY colocar entre f{} # text = ft.Text(camp_send_msg.value) #pega o valor que esta no campo enviar msg, eh o texto que o usuario escreveu. # cria texto dinamico
+                # chat.controls.append(text) # adicionar um elemento no chat, controls.append() isso adiciona um item no final, sempre add item no final
+
+
+
     # funcao completa para enviar a msg
     def send_message(evento): # uma funcao que recebe um evento
         #preencher texto campo msg e nome do user
         name_user = box_name_user.value # pega o valor do nome do usuario
         text_camp_msg = camp_send_msg.value # pega valores campo da msg
-        text = ft.Text(f"{name_user} : {text_camp_msg}") #colocar valores dinamicos no PY colocar entre f{} # text = ft.Text(camp_send_msg.value) #pega o valor que esta no campo enviar msg, eh o texto que o usuario escreveu. # cria texto dinamico
-        chat.controls.append(text) # adicionar um elemento no chat, controls.append() isso adiciona um item no final, sempre add item no final
+        msg = f"{name_user} : {text_camp_msg}"  # vou enviar essa msg no tunel de comunicacao, passo 3
+        # passo 3 do tunel trocado a inf
+        # text = ft.Text(f"{name_user} : {text_camp_msg}") #colocar valores dinamicos no PY colocar entre f{} # text = ft.Text(camp_send_msg.value) #pega o valor que esta no campo enviar msg, eh o texto que o usuario escreveu. # cria texto dinamico
+        # chat.controls.append(text) # adicionar um elemento no chat, controls.append() isso adiciona um item no final, sempre add item no final
         # esse codigo pode ser  ou como acima
         # text = camp_send_msg.value #pega o valor que esta no campo enviar msg, eh o texto que o usuario escreveu
         # chat.controls.append(ft.Text(text)) # adicionar um elemento no chat, controls.append() isso adiciona um item no final, sempre add item no final
+        pagina.pubsub.send_all(msg) # enviar para TODOS os usuarios que estao conectados a msg, envia msg no tunel de comunicacao
         camp_send_msg.value = ""  # limpa a caixa de mensagem
         pagina.update() #sempre atualizar a pagina, e aparece a msg
 
@@ -48,8 +73,10 @@ def main(pagina):  # Costuma dar nome 'main' para a função principal. Obrigato
 
         # aparecer no chat a msg "Name....entrou no chat"
         name_user = box_name_user.value
-        text_msg = ft.Text( f"{name_user} start chat 🧐" )
-        chat.controls.append(text_msg) # add text no chat sempre que user entrar no chat
+        msg = f"{name_user} start chat 🧐" # msg para todos os users - passo 3 msg do tunel de comunicacao, para todos os users
+        #text_msg = ft.Text( f"{name_user} start chat 🧐" )
+        #chat.controls.append(text_msg) # add text no chat sempre que user entrar no chat
+        pagina.pubsub.send_all(msg)
 
         pagina.update()  # Sempre que fizer algo visual na tela, sempre colocar esse comando
 
